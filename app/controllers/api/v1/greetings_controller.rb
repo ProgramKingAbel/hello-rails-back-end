4e882a0cc4e103efcf8fd1e7a7755c5cf2289e22
ProgramKingAbel <abelmorara254@gmail.com>
2023-10-03 22:42:36 +0300

    Implement the random method

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::GreetingsController < ApplicationController
   def random
+    random_greeting = Greeting.order("RANDOM()").first
+    render json: { greeting: random_greeting&.greeting }
   end
 end
