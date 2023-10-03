e6740e82589f98ace691c208801eb0b1d2d10da3
ProgramKingAbel <abelmorara254@gmail.com>
2023-10-03 22:46:46 +0300

    Fix routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'greetings/random', to: 'greetings#random'
+      get 'random_greeting', to: 'greetings#random'
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
